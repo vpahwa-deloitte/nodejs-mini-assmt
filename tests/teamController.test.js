32bab36dc2bc5b356187769f8c799bf84efe296f
Vanshaj Pahwa <vpahwa@deloitte.com>
2023-07-14 14:09:42 +0530

    test: Add unit tests for TeamController using Jest

---
+++ tests/teamController.test.js
@@ -0,0 +1,85 @@
+const teamController = require('../controllers/teamController');
+const { teams } = require('../utils/data');
+const Team = require('../models/team');
+
+describe('Team Controller', () => {
+  describe('getAllTeams', () => {
+    it('should return all teams', () => {
+      const req = {};
+      const res = {
+        json: jest.fn(),
+      };
+
+      teamController.getAllTeams(req, res);
+
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Success',
+        data: teams,
+      });
+    });
+  });
+
+  describe('getTeamById', () => {
+    it('should return the specified team', () => {
+      const teamId = 1;
+      const req = {
+        params: {
+          id: teamId,
+        },
+      };
+      const res = {
+        json: jest.fn(),
+      };
+
+      teamController.getTeamById(req, res);
+
+      const expectedTeam = teams.find((team) => team.id === teamId);
+
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Success',
+        data: expectedTeam,
+      });
+    });
+
+    it('should return an error message if the team is not found', () => {
+      const teamId = 100;
+      const req = {
+        params: {
+          id: teamId,
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      teamController.getTeamById(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Team not found',
+      });
+    });
+  });
+
+  describe('deleteTeam', () => {
+    it('should delete the specified team', () => {
+      const teamId = 1;
+      const req = {
+        params: {
+          id: teamId,
+        },
+      };
+      const res = {
+        json: jest.fn(),
+      };
+
+      teamController.deleteTeam(req, res);
+
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Team deleted',
+      });
+      expect(teams).not.toContainEqual(expect.objectContaining({ id: teamId }));
+    });
+  });
+});
